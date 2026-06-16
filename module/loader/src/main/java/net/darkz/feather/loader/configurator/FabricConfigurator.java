cbc1755c22d662a7f9bc16de1870bf995e7df1aa
darkz70 <d91313250@gmail.com>
2026-06-16 12:04:20 +0200

    Update FabricConfigurator.java

--- module/loader/src/main/java/net/darkz/feather/loader/configurator/FabricConfigurator.java
+++ module/loader/src/main/java/net/darkz/feather/loader/configurator/FabricConfigurator.java
@@ -3,19 +3,6 @@ package net.darkz.feather.loader.configurator;
 import net.darkz.feather.common.FeatherExtension;
 import org.gradle.api.Project;
 
-/**
- * Configures <a href="https://github.com/FabricMC/fabric-loom">Fabric Loom</a>
- * for the target project.
- *
- * <p>Expected {@code feather} block:
- * <pre>{@code
- * feather {
- *     loader          = "fabric"
- *     minecraftVersion = "1.21.1"
- *     loaderVersion   = "0.16.2"   // Fabric Loader version
- * }
- * }</pre>
- */
 public class FabricConfigurator implements LoaderConfigurator {
 
     private static final String LOOM_PLUGIN_ID = "fabric-loom";
@@ -25,22 +12,47 @@ public class FabricConfigurator implements LoaderConfigurator {
         String mc     = ext.getMinecraftVersion().getOrElse("");
         String loader = ext.getLoaderVersion().getOrElse("");
         requireProperty(ext, "minecraftVersion", mc);
-        requireProperty(ext, "loaderVersion",    loader);
+        requireProperty(ext, "loaderVersion", loader);
 
-        // Apply Fabric Loom (must be in the project's buildscript classpath)
         project.getPluginManager().apply(LOOM_PLUGIN_ID);
 
-        // Wire Minecraft & Fabric Loader versions into the standard dependencies
         project.afterEvaluate(p -> {
             p.getDependencies().add("minecraft", "com.mojang:minecraft:" + mc);
-            p.getDependencies().add("mappings",  p.getDependencies()
-                    .create("net.fabricmc:yarn:" + mc + "+build.1:v2"));
-            p.getDependencies().add("modImplementation",
-                    "net.fabricmc:fabric-loader:" + loader);
+
+            // Mojang маппинги вместо Yarn
+            Object loom = p.getExtensions().findByName("loom");
+            if (loom != null) {
+                try {
+                    Object mojangMappings = loom.getClass()
+                            .getMethod("officialMojangMappings")
+                            .invoke(loom);
+                    p.getDependencies().add("mappings", mojangMappings);
+                } catch (Exception e) {
+                    p.getLogger().warn("[FeatherPlugin/Fabric] Mojang mappings error: " + e.getMessage());
+                    // Фоллбек на Yarn
+                    p.getDependencies().add("mappings",
+                            p.getDependencies().create("net.fabricmc:yarn:" + mc + "+build.1:v2"));
+                }
+            }
+
+            p.getDependencies().add("modImplementation", "net.fabricmc:fabric-loader:" + loader);
+
+            // Fabric API
+            String fabricApi = ext.getFabricApi().getOrElse("");
+            if (!fabricApi.isEmpty() && !fabricApi.equals("unknown")) {
+                p.getDependencies().add("modImplementation",
+                        "net.fabricmc.fabric-api:fabric-api:" + fabricApi);
+            }
+
+            // Lombok
+            String lombok = ext.getLombok().getOrElse("");
+            if (!lombok.isEmpty() && !lombok.equals("unknown")) {
+                p.getDependencies().add("compileOnly", "org.projectlombok:lombok:" + lombok);
+                p.getDependencies().add("annotationProcessor", "org.projectlombok:lombok:" + lombok);
+            }
 
             project.getLogger().lifecycle(
-                    "[FeatherPlugin/Fabric] minecraft=" + mc +
-                    " fabricLoader=" + loader);
+                    "[FeatherPlugin/Fabric] minecraft=" + mc + " fabricLoader=" + loader);
         });
     }
 }
