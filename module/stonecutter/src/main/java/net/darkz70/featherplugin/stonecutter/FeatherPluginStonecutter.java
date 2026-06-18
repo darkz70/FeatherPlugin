13a38c8614a756f5908c173554c8e67b9036dfec
darkz69i42k <d91313250@gmail.com>
2026-06-18 15:28:47 +0200

    Update FeatherPluginStonecutter.java

--- module/stonecutter/src/main/java/net/darkz70/featherplugin/stonecutter/FeatherPluginStonecutter.java
+++ module/stonecutter/src/main/java/net/darkz70/featherplugin/stonecutter/FeatherPluginStonecutter.java
@@ -22,7 +22,7 @@ public class FeatherPluginStonecutter implements Plugin<Project> {
 
 	@Override
 	public void apply(@NotNull Project project) {
-		project.getPlugins().apply("dev.kikugie.stonecutter");
+		project.getPlugins().apply("dev.kikugie.stonecutter-controller");
 		Map<String, Project> childProjects = project.getChildProjects();
 		TaskContainer tasks = project.getTasks();
 		StonecutterControllerExtension controller = project.getExtensions().getByType(StonecutterControllerExtension.class);
