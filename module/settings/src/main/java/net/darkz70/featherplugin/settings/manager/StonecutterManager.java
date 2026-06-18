08ba4c97feb734ac69cf8d4fba28581fbf72007e
darkz69i42k <d91313250@gmail.com>
2026-06-18 19:03:21 +0200

    Update StonecutterManager.java

--- module/settings/src/main/java/net/darkz70/featherplugin/settings/manager/StonecutterManager.java
+++ module/settings/src/main/java/net/darkz70/featherplugin/settings/manager/StonecutterManager.java
@@ -9,6 +9,30 @@ import org.jetbrains.annotations.NotNull;
 public class StonecutterManager {
 
 	public static void apply(@NotNull Settings settings, Map<String, List<String>> projects) {
+		settings.getPluginManagement().repositories(repos -> {
+			repos.maven(maven -> {
+				maven.setName("FeatherPluginGitHubPackages");
+				maven.setUrl("https://maven.pkg.github.com/darkz70/FeatherPlugin");
+				maven.credentials(creds -> {
+					String user = System.getenv("GPR_USER");
+					String key = System.getenv("GPR_KEY");
+					if (user == null) user = System.getenv("GITHUB_ACTOR");
+					if (key == null) key = System.getenv("GITHUB_TOKEN");
+					creds.setUsername(user);
+					creds.setPassword(key);
+				});
+			});
+			repos.maven(maven -> {
+				maven.setName("KikuReleases");
+				maven.setUrl("https://maven.kikugie.dev/releases");
+			});
+			repos.maven(maven -> {
+				maven.setName("KikuSnapshots");
+				maven.setUrl("https://maven.kikugie.dev/snapshots");
+			});
+			repos.gradlePluginPortal();
+		});
+
 		settings.getPlugins().apply("dev.kikugie.stonecutter");
 		StonecutterSettingsExtension stonecutter = settings.getExtensions().getByType(StonecutterSettingsExtension.class);
 		stonecutter.create(settings.getRootProject(), (builder) -> {
