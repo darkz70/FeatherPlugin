c844b23b75afec71c181028528848058a9b53f11
darkz69i42k <d91313250@gmail.com>
2026-06-19 05:47:27 +0200

    Update FeatherProjectConfigurationData.java

--- module/core/src/main/java/net/darkz70/featherplugin/core/data/FeatherProjectConfigurationData.java
+++ module/core/src/main/java/net/darkz70/featherplugin/core/data/FeatherProjectConfigurationData.java
@@ -2,6 +2,7 @@ package net.darkz70.featherplugin.core.data;
 
 import net.darkz70.featherplugin.common.FeatherUtils;
 import net.darkz70.featherplugin.core.FeatherPluginCore;
+import net.darkz70.featherplugin.core.extension.FeatherCoreDependenciesExtension;
 import net.darkz70.featherplugin.core.loader.LoaderManager;
 import org.gradle.api.Project;
 import org.jetbrains.annotations.NotNull;
@@ -13,15 +14,16 @@ public record FeatherProjectConfigurationData(
 		String minecraftVersion,
 		String comparableMinecraftVersion,
 		LoaderManager loaderManager,
-		Project project
+		Project project,
+		FeatherCoreDependenciesExtension feather
 ) {
 
-	public static FeatherProjectConfigurationData create(@NotNull Project project, FeatherPluginCore plugin) {
+	public static FeatherProjectConfigurationData create(@NotNull Project project, FeatherPluginCore plugin, FeatherCoreDependenciesExtension feather) {
 		String projectName = project.getName();
 		String loaderName = FeatherUtils.substringBefore(projectName, "-");
 		String minecraftVersion = FeatherUtils.substringSince(projectName, "-");
 		String comparableMinecraftVersion = FeatherUtils.substringBefore(minecraftVersion, "-");
 		LoaderManager loaderManager = LoaderManager.of(loaderName);
-		return new FeatherProjectConfigurationData(plugin, projectName, loaderName, minecraftVersion, comparableMinecraftVersion, loaderManager, project);
+		return new FeatherProjectConfigurationData(plugin, projectName, loaderName, minecraftVersion, comparableMinecraftVersion, loaderManager, project, feather);
 	}
 }
